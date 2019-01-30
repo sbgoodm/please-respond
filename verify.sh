1ad2e817e6a1ffb05cdac371137611f0694f90a2
Stephen Goodman <sgoodman@eitccorp.com>
2019-01-29 22:54:17 -0500

    One more verify attempt.

--- verify.sh
+++ verify.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-grep -q 'total,future_date,future_link,co_1,co_1_count,co_2,co_2_count,co_3,co_3_count'
-grep -qE '\d{1,4},\d{4}-\d{2}-\d{2} \d{1,2}:\d{1,2},https\:\/\/www\.meetup\.com\/.*\/,[a-z]{2},\d{1,4},[a-z]{2},\d{1,4},[a-z]{2},\d{1,4}'
+grep -P '\d{1,4},\d{4}-\d{2}-\d{2} \d{1,2}:\d{1,2},https\:\/\/www\.meetup\.com\/.*\/,[a-z]{2},\d{1,4},[a-z]{2},\d{1,4},[a-z]{2},\d{1,4}' 
+
+if [ $? -eq 0 ]
+then
+  echo "Data format matches!"
+  exit 0
+else
+  echo "Data format does not match expectation."
+  exit 1
+fi
