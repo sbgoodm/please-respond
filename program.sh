cbd942815439c3b40d45aed964a0d78421f5d9b3
Stephen Goodman <sgoodman@eitccorp.com>
2019-01-29 22:18:29 -0500

    Some basic grepping for regex in the CSV output.

---
+++ program.sh
@@ -0,0 +1,3 @@
+echo "total,future_date,future_link,co_1,co_1_count,co_2,co_2_count,co_3,co_3_count"
+echo "100,2019-04-17 12:00,https://www.meetup.com/UXSpeakeasy/events/258247836/,us,40,dk,18,jp,12"
+
--- verify.sh
+++ verify.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo "read stdout and test for matches? Run a diff on file output?"
+grep -q 'total,future_date,future_link,co_1,co_1_count,co_2,co_2_count,co_3,co_3_count'
+grep -qE '\d{1,4},\d{4}-\d{2}-\d{2} \d{1,2}:\d{1,2},https\:\/\/www\.meetup\.com\/.*\/,[a-z]{2},\d{1,4},[a-z]{2},\d{1,4},[a-z]{2},\d{1,4}'
