c2b01a73c1ca52d588f06940dc0d532369364828
Stephen Goodman <sgoodman@eitccorp.com>
2019-01-30 00:16:15 -0500

    Try connecting to meetup for 10s.

--- program.sh
+++ program.sh
@@ -1,5 +1,8 @@
 # This is an example of how your output should be formatted to have the Travis CI build pass.
 # Only the data line is cheecked, the header row is simply for reference
+
+timeout -sHUP 10s curl -i http://stream.meetup.com/2/rsvps
+
 echo "total,future_date,future_url,co_1,co_1_count,co_2,co_2_count,co_3,co_3_count"
 echo "100,2019-04-17 12:00,https://www.meetup.com/UXSpeakeasy/events/258247836/,us,40,dk,18,jp,12"
 
